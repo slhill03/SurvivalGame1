99ff51be3b784b54a57e83d1b3036bdd13088fed
Samuel Hill <47217896+slhill03@users.noreply.github.com>
2019-04-25 14:28:45 -0400

    Add files via upload

--- Game/main.lua
+++ Game/main.lua
@@ -1,7 +1,8 @@
 
 ------------------------------------------------------------------------------
 function love.load()
-backround = love.graphics.newImage("backround.png")
+backround1 = love.graphics.newImage("screen1.png")
+backround2 = love.graphics.newImage("screen2.png")
 nuke = love.graphics.newImage("nuke.png")
 sprite = love.graphics.newImage("sprite1.png")
 x = 330
@@ -11,7 +12,7 @@ v = love.math.random(0, 700)
 health = 100
 font = love.graphics.newFont(20)
   love.graphics.setFont(font)
-
+ time = love.timer.getTime()
 end
 ------------------------------------------------------------------------------
 function love.update()
@@ -37,11 +38,17 @@ end
 function love.draw()
   love.graphics.print(health,-1,20)
   love.graphics.print("Health:")
-love.graphics.draw(backround,0,0,0,1,.75)
+
 love.graphics.draw(sprite,x,y)
 love.graphics.draw(nuke,v,t)
---love.graphics.print(x, 0, 0)
-
+love.graphics.print(x, 0, 0)
+if love.timer.getTime() - time < 30 then  --change time
+   love.graphics.draw(backround1,0,0,0,1,.75)
+ else
+ if love.timer.getTime() - time < 100 then  --change time
+  love.graphics.draw(backround2,0,0,0,1,.75)
+end
+end
 
 end
 
