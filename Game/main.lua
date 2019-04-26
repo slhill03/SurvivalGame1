4da1cdaaf7f7ccefeb60537151f9831dbd03cb9f
Samuel Hill <47217896+slhill03@users.noreply.github.com>
2019-04-26 14:31:06 -0400

    Add files via upload

--- Game/main.lua
+++ Game/main.lua
@@ -2,7 +2,7 @@
 ------------------------------------------------------------------------------
 function love.load()
 backround1 = love.graphics.newImage("screen1.png")
-backround2 = love.graphics.newImage("screen2.png")
+backround2 = love.graphics.newImage("screen2resize.png")
 nuke = love.graphics.newImage("nuke.png")
 sprite = love.graphics.newImage("sprite1.png")
 x = 330
@@ -18,19 +18,28 @@ end
 function love.update()
 
 
---if love.keyboard.isDown('w') then
---  y = y - 25
---end
+if love.keyboard.isDown('right') then
+  x = x + 10
+end
 if love.keyboard.isDown('a') then
   x = x - 10
 end
---if love.keyboard.isDown('s') then
-  --y = y + 25
---end
+if love.keyboard.isDown('left') then
+  x = x - 10
+end
 if love.keyboard.isDown('d') then
   x = x + 10
 end
 t = t + 5
+
+if health == 0
+then love.event.quit()
+end
+
+if cc(x, y, 117, 288, v, t, 144, 189) then
+  health = health - 1
+end
+
 
 
 end
@@ -38,17 +47,19 @@ end
 function love.draw()
   love.graphics.print(health,-1,20)
   love.graphics.print("Health:")
-
+--love.graphics.draw(backround2,0,0,0,1,.75)
 love.graphics.draw(sprite,x,y)
 love.graphics.draw(nuke,v,t)
-love.graphics.print(x, 0, 0)
-if love.timer.getTime() - time < 30 then  --change time
-   love.graphics.draw(backround1,0,0,0,1,.75)
- else
- if love.timer.getTime() - time < 100 then  --change time
-  love.graphics.draw(backround2,0,0,0,1,.75)
-end
-end
+
+--love.graphics.print(x, 0, 0)
+--if love.timer.getTime() - time < 30 then  --change time
+   --love.graphics.draw(backround1,0,0,0,1,.75)
+ --else
+ --if love.timer.getTime() - time < 100 then  --change time
+  --love.graphics.draw(backround2,0,0,0,1,.75)
+--end
+--end
+
 
 end
 
@@ -63,3 +74,10 @@ function love.keyreleased(key)
    end
  end
 ------------------------------------------------------------------------------
+
+function cc(x1,y1,w1,h1, x2,y2,w2,h2)
+  return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1
+end
